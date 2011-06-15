8bedb04d6018989417c0642620a0baf7380bdefa
mmichelli <mario@edison.no>
2011-06-15 22:04:21 +0200

    Fix Batch actions for translated sites

--- code/CMSBatchAction.php
+++ code/CMSBatchAction.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * A class representing back actions 
- * 
+ * A class representing back actions
+ *
  * <code>
- * CMSMain::register_batch_action('publishitems', new CMSBatchAction('doPublish', 
- * 	_t('CMSBatchActions.PUBLISHED_PAGES', 'published %d pages')));
+ * CMSMain::register_batch_action('publishitems', new CMSBatchAction('doPublish',
+ *	_t('CMSBatchActions.PUBLISHED_PAGES', 'published %d pages')));
  * </code>
- * 
+ *
  * @package cms
  * @subpackage batchaction
  */
@@ -16,19 +16,19 @@ abstract class CMSBatchAction extends Object {
 	 * The the text to show in the dropdown for this action
 	 */
 	abstract function getActionTitle();
-	
+
 	/**
 	 * Get text to be shown while the action is being processed, of the form
 	 * "publishing pages".
 	 */
 	abstract function getDoingText();
-	
+
 	/**
 	 * Run this action for the given set of pages.
 	 * Return a set of status-updated JavaScript to return to the CMS.
 	 */
 	abstract function run(DataObjectSet $pages);
-	
+
 	/**
 	 * Helper method for processing batch actions.
 	 * Returns a set of status-updating JavaScript to return to the CMS.
@@ -38,17 +38,19 @@ abstract class CMSBatchAction extends Object {
 	 * @param $helperMethod The method to call on each of those objects.
 	 */
 	public function batchaction(DataObjectSet $pages, $helperMethod, $successMessage, $arguments = array()) {
+
 		$failures = 0;
-		
+
+
 		foreach($pages as $page) {
-			
+
 			// Perform the action
 			if (call_user_func_array(array($page, $helperMethod), $arguments) === false) {
 				$failures++;
 				FormResponse::add("\$('sitetree').addNodeClassByIdx('$page->ID', 'failed');");
 			}
-				
-			
+
+
 			// Now make sure the tree title is appropriately updated
 			$publishedRecord = DataObject::get_by_id('SiteTree', $page->ID);
 			if ($publishedRecord) {
@@ -66,11 +68,11 @@ abstract class CMSBatchAction extends Object {
 		return FormResponse::respond();
 	}
 
-	
+
 
 	/**
-	 * Helper method for applicablePages() methods.  Acts as a skeleton implementation.
-	 * 
+	 * Helper method for applicablePages() methods.	 Acts as a skeleton implementation.
+	 *
 	 * @param $ids The IDs passed to applicablePages
 	 * @param $methodName The canXXX() method to call on each page to check if the action is applicable
 	 * @param $checkStagePages Set to true if you want to check stage pages
@@ -79,12 +81,12 @@ abstract class CMSBatchAction extends Object {
 	function applicablePagesHelper($ids, $methodName, $checkStagePages = true, $checkLivePages = true) {
 		if(!is_array($ids)) user_error("Bad \$ids passed to applicablePagesHelper()", E_USER_WARNING);
 		if(!is_string($methodName)) user_error("Bad \$methodName passed to applicablePagesHelper()", E_USER_WARNING);
-		
+
 		$applicableIDs = array();
-		
+
 		$SQL_ids = implode(', ', array_filter($ids, 'is_numeric'));
 		$draftPages = DataObject::get("SiteTree", "\"SiteTree\".\"ID\" IN ($SQL_ids)");
-		
+
 		$onlyOnLive = array_fill_keys($ids, true);
 		if($checkStagePages) {
 			foreach($draftPages as $page) {
@@ -92,25 +94,26 @@ abstract class CMSBatchAction extends Object {
 				if($page->$methodName()) $applicableIDs[] = $page->ID;
 			}
 		}
-		
+
 		// Get the pages that only exist on live (deleted from stage)
 		if($checkLivePages && $onlyOnLive) {
 			$SQL_ids = implode(', ', array_keys($onlyOnLive));
 			$livePages = Versioned::get_by_stage("SiteTree", "Live", "\"SiteTree\".\"ID\" IN ($SQL_ids)");
-		
+
 			if($livePages) foreach($livePages as $page) {
 				if($page->$methodName()) $applicableIDs[] = $page->ID;
 			}
 		}
 
+
 		return $applicableIDs;
 	}
-	
+
 	// if your batchaction has parameters, return a fieldset here
 	function getParameterFields() {
 		return false;
 	}
-	
+
 	/**
 	 * If you wish to restrict the batch action to some users, overload this function.
 	 */
@@ -121,7 +124,7 @@ abstract class CMSBatchAction extends Object {
 
 /**
  * Publish items batch action.
- * 
+ *
  * @package cms
  * @subpackage batchaction
  */
@@ -134,19 +137,21 @@ class CMSBatchAction_Publish extends CMSBatchAction {
 	}
 
 	function run(DataObjectSet $pages) {
+
 		return $this->batchaction($pages, 'doPublish',
 			_t('CMSBatchActions.PUBLISHED_PAGES', 'Published %d pages, %d failures')
 		);
 	}
 
 	function applicablePages($ids) {
+
 		return $this->applicablePagesHelper($ids, 'canPublish', true, false);
 	}
 }
 
 /**
  * Delete items batch action.
- * 
+ *
  * @package cms
  * @subpackage batchaction
  */
@@ -160,10 +165,10 @@ class CMSBatchAction_Delete extends CMSBatchAction {
 
 	function run(DataObjectSet $pages) {
 		$failures = 0;
-		
+
 		foreach($pages as $page) {
 			$id = $page->ID;
-			
+
 			// Perform the action
 			if($page->canDelete()) $page->delete();
 			else $failures++;
@@ -198,7 +203,7 @@ class CMSBatchAction_Delete extends CMSBatchAction {
 
 /**
  * Unpublish (delete from live site) items batch action.
- * 
+ *
  * @package cms
  * @subpackage batchaction
  */
@@ -215,7 +220,7 @@ class CMSBatchAction_DeleteFromLive extends CMSBatchAction {
 		$this->batchaction($pages, 'doUnpublish',
 			_t('CMSBatchActions.DELETED_PAGES', 'Deleted %d pages from the published site, %d failures')
 		);
-		
+
 		foreach($ids as $pageID) {
 			$id = $pageID;
 
@@ -232,7 +237,7 @@ class CMSBatchAction_DeleteFromLive extends CMSBatchAction {
 				FormResponse::add("$('Form_EditForm').reloadIfSetTo($id);");
 			}
 		}
-		
+
 		return FormResponse::respond();
 	}
 
